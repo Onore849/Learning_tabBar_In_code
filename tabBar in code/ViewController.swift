6ea30e0b2e65f69b44ac1bb5a28a47a43b06f296
Onore849 <rion3376@gmail.com>
2020-09-16 14:40:51 +0900

    Initial Commit

---
+++ tabBar in code/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tabBar in code
+//
+//  Created by 野澤拓己 on 2020/09/16.
+//  Copyright © 2020 Takumi Nozawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
